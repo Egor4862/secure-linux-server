3b0aa06cb981effe309b4ce7d8565bde92c0acab
Егор <egor.timoshenko2007@gmail.com>
2026-07-07 17:24:41 +0400

    iptables-rules.sh

--- iptables-rules.sh
+++ iptables-rules.sh
@@ -1,10 +1,10 @@
-# Дропаем NULL пакеты, обычно такие пакеты прилетают, когда вас сканируют.
+# NULL пакеты
 iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
 
-# Дропаем syn flood attack
+# Syn flood attack
 iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
 
-# Блокируем Christmas tree аттаку. Это пакет, который содержит все опции (SYN, FIN, URG and PSH) такие пакеты обрабатываются намного дольше. Такое легко закрывается так:
+# Блокируем Christmas tree аттаку
 iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
 
 # HTTP/HTTPS
@@ -19,29 +19,29 @@ iptables -A INPUT -i ens33 -p tcp --dport 22 -m state --state NEW -m recent --up
 iptables -A INPUT -i ens33 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o ens33 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
-# Разрешить выход наружу по протокола HTTP/HTTPS (80/443)
+# HTTP/HTTPS (80/443)
 iptables -A INPUT -i ens33 -p tcp --sport 80 -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -i ens33 -p tcp --sport 443 -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -o ens33 -p tcp --dport 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -o ens33 -p tcp --dport 443 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
-# Разрешим DNS траффик
+# DNS траффик
 iptables -A INPUT -i ens33 -p udp --sport 53 -m state --state ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o ens33 -p udp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
 
-# Разрешим все локальные подключения
+# Локальные подключения
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
 # Логирование
-# Создадим все новые цепочки
+# Создание новых цепочек
 iptables -N LOGINPUT
 iptables -N LOGOUTPUT
-# добавим новые правила для новых цепочек INPUT и OUTPUT и перенаправим правила в LOGINPUT и LOGOUTPUT
+# Правила для новых цепочек INPUT и OUTPUT и перенаправление правила в LOGINPUT и LOGOUTPUT
 iptables -A INPUT -j LOGINPUT
 iptables -A OUTPUT -j LOGOUTPUT
-# модуль limit нужен, чтобы писать событие не чаще раза в 4 минуты. log-level4=Warning
-# логи будут записаны в /var/log/messages (–log)
+# Модуль limit
+# Логи будут записаны в /var/log/messages (–log)
 iptables -A LOGINPUT -m limit --limit 4/min -j LOG --log-prefix "DROP INPUT: " --log-level 4
 iptables -A LOGOUTPUT -m limit --limit 4/min -j LOG --log-prefix "DROP OUTPUT: " --log-level 4
 
